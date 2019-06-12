f376e382f726a06e236ad4d712ff61286f9f813e
Md. Mahamudun Hassan <mahamudun013@gmail.com>
2019-06-13 00:04:27 +0600

    Mobile object tracking in a video using Kalman filter
    Mobile object tracking in a video using Kalman filter.

---
+++ Car Tracking Matlab Code/CarTracking_imageProccessing.m
@@ -0,0 +1,117 @@
+%% This code for process the images for object tracking .
+
+clear all; % free up system memory
+close all; % removed specified figure
+
+% creat the frames address as a directory
+directory = 'D:\Matlab-2013a\bin\ALL_TRACKING\realcar2 (29-Jul-17 4-43-48 AM)';
+
+% by using this command enter the directory
+cd(directory);
+
+% for get the list of frames
+frame_list =  dir('*jpg');
+
+%%Make average of background images
+
+N = 30; % num of frames to use for averaged background
+
+%define image stack for averaging
+image_avg = zeros(480,640,N); 
+
+for i = 1:N   
+    
+    image_temp = imread(frame_list(i).name); % for reading given image
+    image_avg(:,:,i) =image_temp(:,:,1); % use first dimension of the image
+
+end
+
+background_image = (mean(image_avg,3)); % take the average image
+subplot(121);
+imagesc(background_image)
+
+subplot(122);
+imagesc(image_avg(:,:,1))
+colormap(gray)
+
+%gaussian filter initialization
+hsize = 20;
+sigma = 10;
+gaussian_filter = fspecial('gaussian',hsize, sigma);
+
+subplot(121);
+imagesc(gaussian_filter)
+subplot(122);
+mesh(gaussian_filter)
+colormap(gray)
+
+%for making the coordinate locations more visible.
+SE = strel('diamond', 7);
+
+% initialize the variable that will store the object locations(x,y)
+Track_data = zeros(length(frame_list),2);
+
+% iteratively find the object
+for i = 1:length(frame_list) 
+   
+    image_temp = double(imread(frame_list(i).name)); % load the image and convert it into double for computation. 
+    image = image_temp(:,:,1); % reduce image in first dimension
+    
+    subplot(221);
+    imagesc(image);
+    title('Original');
+  
+    %subtract background from the image
+    subtract_image = (image - background_image);
+    subplot(222);
+    imagesc(subtract_image);
+    title('Background subtracted');
+    
+    %gaussian filtering for image
+    gaussian_image = filter2(gaussian_filter,subtract_image,'same');     
+    subplot(223);imagesc(gaussian_image);
+    title('Gaussian filter smoothed');
+    
+    %threshold the image
+    subplot(224);
+    hist(gaussian_image(:)); % create histogram
+    threshold_image = (gaussian_image < -30);
+    
+    subplot(224);
+    imagesc(threshold_image);
+    title('Thresholded');
+   
+    
+    % Tracking object and finding center of the object
+  
+    [x,y] = find (threshold_image);   
+    if ~isempty(x)
+        
+        Track_data(i,:) = ceil([mean(x) mean(y)]+1); % ceiling to avoid zero indices
+   
+    else
+        
+        Track_data(i,:) =1;
+        
+    end
+ 
+  
+    % make binary image with single coordinate of object
+    object_image = zeros(size(threshold_image)); 
+    object_image(Track_data(i,1),Track_data(i,2)) = 1;
+    %figure, imshow(object_image);
+
+    object_image = imdilate(object_image, SE);
+    subplot(224);
+    imagesc(threshold_image + object_image);
+    title('thresholded and detect centroid');
+    
+
+    pause(.001)
+end
+
+%save the coordinates 
+save('Track_data_store.mat', 'Track_data');
+
+
+     
---
+++ Person Tracking Matlab code/kalman_filter.m
@@ -0,0 +1,149 @@
+%%This code is for applying kalman filter.
+
+clear all;  %free up system memory
+close all;
+clc; 
+
+% creat the frames address as a directory
+directory = 'D:\Matlab-2013a\bin\ALL_TRACKING\Person_trk\person (26-Jul-17 6-18-32 PM)';
+
+%by using this command enter the directory
+cd(directory);
+
+% for get the list of frames
+frame_list =  dir('*jpg');
+
+% for load tracking data
+load('Track_data_store.mat');
+
+%% define variables for kalman filter
+
+dt = 1;  % sampling rate
+
+Starting_frame = 10; % starting frame
+
+u = .005;  % define acceleration magnitude
+
+Q = [Track_data(Starting_frame,1); Track_data(Starting_frame,2); 0; 0]; % initized-[positionX; positionY; velocityX; velocityY] of the object
+
+Q_estimate = Q;  % estimate of initial location estimation of where the object is.
+
+Accel_noise_mag = .1; % process noise
+
+noise_x = 1;  % measurement noise(x axis).
+noise_y = 1;  % measurement noise(y axis).
+
+Ez = [noise_x 0; 0 noise_y];
+
+Ex = [dt^4/4 0 dt^3/2 0; ...
+      0 dt^4/4 0 dt^3/2; ...
+      dt^3/2 0 dt^2 0; ...
+      0 dt^3/2 0 dt^2].*Accel_noise_mag^2; % convert the process noise into covariance matrice
+  
+P = Ex;  % estimate of initial object position variance
+
+%% Define update equations in 2-D
+
+A = [1 0 dt 0; 0 1 0 dt; 0 0 1 0; 0 0 0 1]; % state update matrice
+B = [(dt^2/2); (dt^2/2); dt; dt];
+C = [1 0 0 0; 0 1 0 0];  % this is measurement function C, that apply to the state estimate Q to get expect next measurement
+
+
+%%Initialize result variables
+
+Q_loc = []; % actual object motion path
+vel = [];  % actual object velocity
+Q_location_measurement = []; % the objectpath extracted by the tracking algorithm
+
+
+%% Initize estimation variables
+
+Q_location_estimate = []; % position estimate
+velocity_estimate = []; % velocity estimate
+P_estimate = P;
+predic_state = [];
+predic_var = [];
+
+r = 15; % define radius for the plotting circle
+j=0:.01:2*pi; % for make the plotting circle
+
+
+for t = Starting_frame:length(frame_list)
+    
+    % loading image
+    img_tmp = double(imread(frame_list(t).name));
+    img = img_tmp(:,:,1);
+    subplot(121);imagesc(img);
+    axis off
+    title('Raw Video');
+    
+    % Load the given tracking
+
+    Q_location_measurement(:,t) = [ Track_data(t,1); Track_data(t,2)];  %measurement value from image processing 
+
+    
+    %% Applying of kalman filter   
+    
+    % Predict next state of the object with the last state and predicted motion.
+    Q_estimate = A * Q_estimate + B * u;
+    predic_state = [predic_state; Q_estimate(1)] ; 
+    
+    % Predict next covariance
+    P = A * P * A' + Ex;
+    predic_var = [predic_var; P] ;
+    
+    % Kalman gain
+    K = P*C'*inv(C*P*C'+Ez);% P=P(predict),Ez=measurement noise covarience
+    
+    % Update the state estimate.
+
+    if ~isnan(Q_location_measurement(:,t))  % Not a number(not zero)check
+        
+        Q_estimate = Q_estimate + K * (Q_location_measurement(:,t) - C * Q_estimate); %Updated real position
+        
+    end
+
+    
+    % Update error covariance estimation.
+    P =  (eye(4)-K*C)*P;
+    
+    % Store data
+    Q_location_estimate = [Q_location_estimate; Q_estimate(1:2)];
+    velocity_estimate = [velocity_estimate; Q_estimate(3:4)];
+   
+    
+    % Plot the images with the tracking
+    subplot(122);
+    imagesc(img);
+    axis off
+    colormap(gray);
+    
+    hold on;
+    
+    plot(r*sin(j)+Q_estimate(2),r*cos(j)+Q_estimate(1),'.r'); % The kalman filtered tracking
+    
+    %plot(r*sin(j)+Q_location_measurement(2,t),r*cos(j)+Q_location_measurement(1,t),'.g'); % The measurement tracking
+    
+    hold off;
+    
+    title('Object Tracked');
+  
+    pause(0.0001)
+end
+
+for t = 1:length(frame_list)
+   
+   hold on;
+   
+   plot(Track_data(:,2),Track_data(:,1),'.b');
+ 
+   hold off
+   
+end
+
+figure;
+plot(Track_data(:,2),Track_data(:,1),'.b');
+xlabel('X-Axis');
+ylabel('Y-Axis');
+title(' Object traveling path')
+
